c8d3b570931687a79c4a8036a464b8c6e8c4f88b
Kavin Shanbhag <kavinplays@icloud.com>
2021-07-14 23:25:27 +0530

    making it work

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: require('@sveltejs/adapter-static')(),
+const sveltePreprocess = require('svelte-preprocess');
+const pkg = require('./package.json');
+
+module.exports = {
+    kit: {
+	    adapter: require('@sveltejs/adapter-static')(),
 		target: '#svelte'
 	}
 };
-
-export default config;
