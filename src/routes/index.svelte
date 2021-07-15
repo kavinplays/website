18aa89f5d32c3a7428156e2cb9f0ea407cbb42b0
Kavin Shanbhag <kavinplays@icloud.com>
2021-07-15 22:47:04 +0530

    checking for 404s

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<link rel="stylesheet" href="index.css">
+<p>Bruh!</p>
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+        margin: auto;
+        text-align: center;
+	}
+</style>
