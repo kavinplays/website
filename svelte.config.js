1e8c019d578e3a9b0594173d2cc9646889d0c1a1
Kavin Shanbhag <kavinplays@icloud.com>
2021-07-15 14:23:52 +0530

    finally

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
+// then in the config object, add an adapter key under kit, and call the imported library
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
-
 export default config;
