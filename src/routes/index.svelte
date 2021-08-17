358d92474696937bba587d0438488bd7b86b7c94
Kavin Shanbhag <kavinplays@icloud.com>
2021-08-17 17:08:37 +0530

    about page testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-gray-500 h-screen justify-center">
+<center class="mt-52 m-auto">
+
+<div class=" tracking-widest"><br>
+<span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
+<span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
+<span class="text-gray-500 text-xl block">You can still use the public api at</span>
+
+</div>
+</center>
+<center class="py-6">
+<a href="https://kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
+</center>
+</div>
