b5d4b665ce53f0ad51ec9800a7ea33a2af953b6a
Kavin Shanbhag <kavinplays@icloud.com>
2021-08-27 21:28:56 +0530

    or the second

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<svelte:head>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KavinPlays</title>
     <meta name="description" content="HomePage for the website">
     <meta name="keywords" content="kavinplays,kavin">
     <meta name="author" content="kavinplays">
     <!-- <link rel="author" href="humans.txt" /> -->
-</head>
-<body>
-    <div class="text-gray-500 h-screen justify-center">
-    <center class="mt-52 m-auto">
+</svelte:head>
+<div class="text-gray-500 h-screen justify-center">
+<center class="mt-52 m-auto">
 
-    <div class=" tracking-widest"><br>
-    <span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
-    <span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
-    <span class="text-gray-500 text-xl block">You can still use the public api at</span>
+<div class=" tracking-widest"><br>
+<span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
+<span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
+<span class="text-gray-500 text-xl block">You can still use the public api at</span>
 
-    </div>
-    </center>
-    <center class="py-6">
-    <a href="https://kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
-    </center>
-    </div>
-</body>
-</html>
+</div>
+</center>
+<center class="py-6">
+<a href="https://kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
+</center>
+</div>
