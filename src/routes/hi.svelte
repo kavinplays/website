8a6c7ad8a576d832e891f91d9af6484c493dee4b
Kavin Shanbhag <kavinplays@icloud.com>
2021-08-27 21:17:44 +0530

    seo changes

--- src/routes/hi.svelte
+++ src/routes/hi.svelte
@@ -1,65 +1,18 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-    <div class="flex-1 flex justify-between sm:hidden">
-      <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-        Previous
-      </a>
-      <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-        Next
-      </a>
-    </div>
-    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-      <div>
-        <p class="text-sm text-gray-700">
-          Showing
-          <span class="font-medium">1</span>
-          to
-          <span class="font-medium">10</span>
-          of
-          <span class="font-medium">97</span>
-          results
-        </p>
-      </div>
-      <div>
-        <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-          <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-            <span class="sr-only">Previous</span>
-            <!-- Heroicon name: solid/chevron-left -->
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-            </svg>
-          </a>
-          <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-          <a href="#" aria-current="page" class="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-            1
-          </a>
-          <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-            2
-          </a>
-          <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium">
-            3
-          </a>
-          <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
-            ...
-          </span>
-          <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium">
-            8
-          </a>
-          <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-            9
-          </a>
-          <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-            10
-          </a>
-          <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-            <span class="sr-only">Next</span>
-            <!-- Heroicon name: solid/chevron-right -->
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-            </svg>
-          </a>
-        </nav>
-      </div>
-    </div>
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a href="https://kavinplays.ml" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24"> -->
+      <img src="/favicon.ico" class="inline-flex items-center w-16">
+      <!-- </svg> -->
+      <span class="ml-3 text-xl" >KavinPlays</span>
+    </a>
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <a href="https://kavinplays.ml" class="mr-5 hover:text-gray-900">First Link</a>
+      <a href="https://kavinplays.ml" class="mr-5 hover:text-gray-900">Second Link</a>
+      <a href="https://kavinplays.ml" class="mr-5 hover:text-gray-900">Third Link</a>
+      <a href="https://kavinplays.ml" class="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button</button>
   </div>
-  
+</header>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,30 @@
-<div class="text-gray-500 h-screen justify-center">
-<center class="mt-52 m-auto">
+<!DOCTYPE html>
+<html lang="en">
 
-<div class=" tracking-widest"><br>
-<span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
-<span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
-<span class="text-gray-500 text-xl block">You can still use the public api at</span>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KavinPlays</title>
+    <meta name="description" content="HomePage for the website">
+    <meta name="keywords" content="kavinplays,kavin">
+    <meta name="author" content="kavinplays">
+    <link rel="author" href="humans.txt" />
+</head>
+<body>
+    <div class="text-gray-500 h-screen justify-center">
+    <center class="mt-52 m-auto">
 
-</div>
-</center>
-<center class="py-6">
-<a href="https://kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
-</center>
-</div>
+    <div class=" tracking-widest"><br>
+    <span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
+    <span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
+    <span class="text-gray-500 text-xl block">You can still use the public api at</span>
+
+    </div>
+    </center>
+    <center class="py-6">
+    <a href="https://kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
+    </center>
+    </div>
+</body>
+</html>
