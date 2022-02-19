e6c9bd94253d8aaacf21eee1588857d60df375aa
Kavin Shanbhag <kavinplays@icloud.com>
2022-02-19 23:45:56 +0530

    added zoom on hover for buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,18 @@
 <svelte:head>
     <title>KavinPlays</title>
 </svelte:head>
-<div class="text-gray-500 h-screen justify-center">
-<center class="mt-52 m-auto">
+<div class="text-gray-500 h-screen justify-center overflow-hidden">
+<center class="mt-56 m-auto">
 
 <div class=" tracking-widest"><br>
 <span class="text-gray-500 text-6xl block"><span>Not Ready Yet!</span></span><br>
 <span class="text-gray-500 text-xl block">Sorry, the site currently isn't ready for production</span>
-<span class="text-gray-500 text-xl block">You can still use the public api at</span>
-
+<span class="text-gray-500 text-xl block">You can still use the public api and checkout the docs</span>
 </div>
 </center>
 <center class="py-6">
-<a href="https://www.kavinplays.ml/api" class="text-gray-500 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md">API</a>
-</center>
+<div class="flex h-1/6 justify-center text-gray-500">
+<a href="https://www.kavinplays.ml/api" class="mx-4 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md hover:scale-125">API</a>
+<a href="https://www.kavinplays.ml/docs" class="mx-4 font-mono text-xl bg-gray-200 p-3 rounded-md hover:shadow-md hover:scale-125">DOCS</a>
+</div>
 </div>
